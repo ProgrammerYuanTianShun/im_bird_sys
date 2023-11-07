4b1491b52b16bd2385263ca40554d8fc2a2bfce4
ProgrammerYuanTianShun <91565242+ProgrammerYuanTianShun@users.noreply.github.com>
2023-11-07 21:14:39 +0800

    pull test

--- src/main/java/org/luminary/controller/GitController.java
+++ src/main/java/org/luminary/controller/GitController.java
@@ -12,5 +12,6 @@ public class GitController {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+         System.out.println("pull test");
     }
 }
